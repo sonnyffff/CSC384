6c298ee099db25c635fd3353192bfec085c191be
Sonny <sonnyzijia.chen@mail.utoronto.ca>
2023-03-26 18:01:10 -0400

    tagger three tables implemented with ambiguity checks and end sentence check

--- A4/tagger.py
+++ A4/tagger.py
@@ -3,12 +3,26 @@ import sys
 import argparse
 
 init_prob_table = {}
+# key: pos, value: dict{pos, probability}
+trans_prob_table = {}
+# observation probability P(E | S)
+observe_prob_table = {}
+
+
+def translate_ambiguity(pos: str):
+    if '-' in pos:
+        parts = pos.split('-')
+        return parts[1] + '-' + parts[0]
 
 
 def read_files(training_list: list):
     init_occurrence = {}
+    transition = {}
+    observation = {}
     prev_word = ' '
+    prev_pos = ' '
     total_sentences = 0
+    total_transitions = 0
     for file in training_list:
         training_file = open(file, "r")
         for line in training_file:
@@ -16,18 +30,70 @@ def read_files(training_list: list):
             new_parts = []
             for part in parts:
                 new_parts.append(part.strip())
-            #  appears at the beginning of a sentence
-            if prev_word == ' ' or prev_word == '.':
+            # appears at the beginning of a sentence
+            if prev_word == ' ' or prev_word == '.' and new_parts[1] not in ['PUL', 'PUQ', 'PUR', 'PUN']:
                 total_sentences += 1
-                if new_parts[1] not in init_occurrence:
+                if new_parts[1] and translate_ambiguity(new_parts[1]) not in init_occurrence:
                     init_occurrence[new_parts[1]] = 1
-                else:
+                elif new_parts[1] in init_occurrence:
                     init_occurrence[new_parts[1]] += 1
-            prev_word = new_parts[0]
-    # calculate initial probability for pos
+                else:
+                    init_occurrence[translate_ambiguity(new_parts[1])] += 1
+            # check previous pos
+            if prev_pos != ' ':
+                if prev_pos and translate_ambiguity(prev_pos) not in transition:
+                    transition[prev_pos] = {new_parts[1]: 1}
+                if prev_pos in transition:
+                    if new_parts[1] and translate_ambiguity(new_parts[1]) not in transition[prev_pos]:
+                        transition[prev_pos][new_parts[1]] = 1
+                    elif new_parts[1] in transition[prev_pos]:
+                        transition[prev_pos][new_parts[1]] += 1
+                    else:
+                        transition[prev_pos][translate_ambiguity(new_parts[1])] += 1
+                else:
+                    if new_parts[1] and translate_ambiguity(new_parts[1]) not in transition[translate_ambiguity(prev_pos)]:
+                        transition[translate_ambiguity(prev_pos)][new_parts[1]] = 1
+                    elif new_parts[1] in transition[prev_pos]:
+                        transition[translate_ambiguity(prev_pos)][new_parts[1]] += 1
+                    else:
+                        transition[translate_ambiguity(prev_pos)][translate_ambiguity(new_parts[1])] += 1
+            # check words
+            if new_parts[1] and translate_ambiguity(new_parts[1]) not in observation:
+                observation[new_parts[1]] = {new_parts[0]: 1}
+            if new_parts[1] in observation:
+                if new_parts[0] not in observation[new_parts[1]]:
+                    observation[new_parts[1]][new_parts[0]] = 1
+                else:
+                    observation[new_parts[1]][new_parts[0]] += 1
+            else:
+                if new_parts[0] not in observation[translate_ambiguity(new_parts[1])]:
+                    observation[translate_ambiguity(new_parts[1])][new_parts[0]] = 1
+                else:
+                    observation[translate_ambiguity(new_parts[1])][new_parts[0]] += 1
+            total_transitions += 1
+            if new_parts[1] not in ['PUL', 'PUQ', 'PUR']:
+                prev_word = new_parts[0]
+                print(prev_word)
+            prev_pos = new_parts[1]
+    # calculate initial probability
     for pos in init_occurrence:
         init_prob_table[pos] = init_occurrence[pos] / total_sentences
-    print(total_sentences)
+    # calculate transition probability
+    for pos in transition:
+        trans_prob_table[pos] = {}
+        for pos2 in transition[pos]:
+            trans_prob_table[pos][pos2] = transition[pos][pos2] / (total_transitions - 1)
+    # calculate observation probability
+    # number of word occurrence base on POS / number of total occurrence base on that POS TODO
+    for pos in observation:
+        observe_prob_table[pos] = {}
+        sample_size = 0
+        for word in observation[pos]:
+            sample_size += observation[pos][word]
+        for word in observation[pos]:
+            observe_prob_table[pos][word] = observation[pos][word] / sample_size
+
+    print(transition)
     return init_occurrence
 
 
@@ -63,5 +129,8 @@ if __name__ == '__main__':
     #
     #
     # print("Starting the tagging process.")
-    print(read_files(['training1.txt']))
+    read_files(['training_simple2.txt'])
     print(init_prob_table)
+    print(trans_prob_table)
+    print(observe_prob_table)
+
